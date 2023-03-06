9d435ad31a139423db3e3aae0f4e06067e6893f9
Gwynneth Uhrhammer <gleece@gmail.com>
2023-03-05 21:21:14 -0800

    WIP hex tile spawning

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -6,42 +6,66 @@ public class GridManager : MonoBehaviour
 {
     private struct AxialCoord
     {
-        public int p;
         public int q;
+        public int r;
 
-        public AxialCoord(int p, int q)
+        public AxialCoord(int q, int r)
         {
-            this.p = p;
             this.q = q;
+            this.r = r;
         }
 
         public override string ToString()
         {
-            return $"({p},{q})";
+            return $"({q},{r})";
         }
 
         public override int GetHashCode()
         {
-            return (p, q).GetHashCode();
+            return (q, r).GetHashCode();
         }
     }
 
     private class HexTile
     {
-        private AxialCoord coord;
+        public AxialCoord AxialCoordinates { get; private set; }
+
+        public GameObject TileObject { get; set; }
 
         public HexTile(AxialCoord coord)
         {
-            this.coord = coord;
+            AxialCoordinates = coord;
         }
     }
+
+    private const int GRID_SIZE = 2;
+
+    [SerializeField]
+    private GameObject tilePrefab;
+
+    [SerializeField]
+    private float horizontalSpacing = 1.5f;
+
+    [SerializeField]
+    private float verticalSpacing = 1.5f;
 
     private Dictionary<AxialCoord, HexTile> tileMap = new Dictionary<AxialCoord, HexTile>();
 
     private void Start()
     {
-        InitializeGrid(2);
+        InitializeGrid(GRID_SIZE);
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ClearGrid();
+            InitializeGrid(GRID_SIZE);
+        }
+    }
+
+    
 
     private void InitializeGrid(int size)
     {
@@ -58,5 +82,35 @@ public class GridManager : MonoBehaviour
                 }
             }
         }
+
+        foreach (var tile in tileMap.Values)
+        {
+            var offsetCoord = AxialToOffsetCoord(tile.AxialCoordinates);
+            
+            var tilePosition = new Vector3(offsetCoord.x * horizontalSpacing, 0, offsetCoord.y * verticalSpacing);
+            if (offsetCoord.y % 2 == 0)
+            {
+                tilePosition.x -= horizontalSpacing / 2;
+            }
+            var tileObject = Instantiate(tilePrefab, tilePosition, Quaternion.identity);
+            tile.TileObject = tileObject;
+        }
+    }
+
+    private void ClearGrid()
+    {
+        foreach (var tile in tileMap.Values)
+        {
+            GameObject.Destroy(tile.TileObject);
+        }
+
+        tileMap.Clear();
+    }
+
+    private Vector2 AxialToOffsetCoord(AxialCoord axialCoord)
+    {
+        var col = axialCoord.q + (axialCoord.r - (axialCoord.r & 1)) / 2;
+        var row = axialCoord.r;
+        return new Vector2(col, row);
     }
 }
