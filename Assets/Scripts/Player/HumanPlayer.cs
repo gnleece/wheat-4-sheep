3b46604aafdfb5cf31945ff6cae7dc44356bd7ec
Gwynneth Uhrhammer <gleece@gmail.com>
2026-03-09 14:05:46 -0700

    Remove debug key presses for player input

--- Assets/Scripts/Player/HumanPlayer.cs
+++ Assets/Scripts/Player/HumanPlayer.cs
@@ -57,53 +57,6 @@ public class HumanPlayer : IPlayer
 
         while (boardManager.IsPlayerTurn(this))
         {
-            // Keep legacy keyboard input as backup for development
-            if (Input.GetKeyDown(KeyCode.Alpha1))
-            {
-                // Roll Dice
-                var diceRoll = await boardManager.RollDice(this);
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha2))
-            {
-                // Build Settlement
-                var chosenSettlementLocation = await boardManager.GetManualSelectionForSettlementLocation(this);
-                boardManager.BuildSettlement(this, chosenSettlementLocation);
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha3))
-            {
-                // Build Road
-                var chosenRoadLocation = await boardManager.GetManualSelectionForRoadLocation(this);
-                boardManager.BuildRoad(this, chosenRoadLocation);
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha4))
-            {
-                // Upgrade Settlement to City
-                var chosenSettlementToUpgrade = await boardManager.GetManualSelectionForSettlementUpgrade(this);
-                boardManager.UpgradeSettlementToCity(this, chosenSettlementToUpgrade);
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha5))
-            {
-                // Buy Development Card
-                if (boardManager.CanBuyDevelopmentCard(this))
-                {
-                    boardManager.BuyDevelopmentCard(this);
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha6))
-            {
-                // Play Development Card
-                if (boardManager.CanPlayAnyDevCard(this))
-                {
-                    var cardType = await boardManager.GetManualDevCardSelection(this);
-                    await boardManager.PlayDevelopmentCard(this, cardType);
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha0))
-            {
-                // End Turn
-                boardManager.EndPlayerTurn(this);
-            }
-
             await Task.Yield();
         }
     }
